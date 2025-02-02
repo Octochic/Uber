1913510eb69ab2193438f05b6be72d2e60c09001
Octochic <sakshikharade.work@gmail.com>
2025-02-02 10:19:38 +0530

    Created create ride route (giving fare, otp etc)

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,12 @@ const connectToDb= require('./db/db')
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
 const app = express();
-const mapsRoutes =require('./routes/maps.routes')
+// app.use(express.json());
+const mapsRoutes =require('./routes/maps.routes');
+const rideRoutes= require('./routes/ride.routes');
+const authMiddleware = require('./middlewares/auth.middleware')
+
+
 
 connectToDb();
 
@@ -27,5 +32,7 @@ app.get('/',(req, res)=>{
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
 
 module.exports=app
---
+++ Backend/services/ride.service.js
@@ -0,0 +1,79 @@
+const crypto = require('crypto');
+
+const rideModel = require('../models/ride.model');
+const mapService = require('./maps.service');
+
+
+
+
+function getOtp(num){
+    function generateOtp(num){
+        const otp= crypto.randomInt(Math.pow(10, num-1), Math.pow(10, num)).toString();
+        return otp;
+    }
+
+    return generateOtp(num);
+}
+
+async function getFare(pickup, destination) {
+    if (!pickup || !destination) {
+        throw new Error('Pickup and destination are required');
+    }
+
+    const distanceTime = await mapService.getDistanceTime(pickup, destination);
+
+    console.log("Distance and Duration:", distanceTime);
+
+    if (!distanceTime || !distanceTime.distance || !distanceTime.duration) {
+        throw new Error("Invalid distance or duration from map service");
+    }
+
+    const distanceKm = distanceTime.distance.value / 1000; // Convert meters to km
+    const durationMin = distanceTime.duration.value / 60; // Convert seconds to minutes
+
+    console.log(`Converted Distance: ${distanceKm} km, Duration: ${durationMin} mins`);
+
+    const RATES = {
+        auto: { base: 25, perKm: 12, perMin: 1 },
+        car: { base: 50, perKm: 15, perMin: 2 },
+        motorcycle: { base: 20, perKm: 10, perMin: 0.5 }
+    };
+
+    const fares = {};
+    for (const vehicle in RATES) {
+        const rate = RATES[vehicle];
+        fares[vehicle] = rate.base + (distanceKm * rate.perKm) + (durationMin * rate.perMin);
+    }
+
+    console.log("Calculated Fares:", fares);
+
+    return fares;
+}
+
+
+
+
+module.exports.createRide = async({
+    user, pickup, destination, vehicleType
+})=>{
+    if(!user|| !pickup|| !destination|| !vehicleType){
+        throw new Error('All feilds are required');
+    }
+
+    const fare = await getFare(pickup, destination);
+
+    console.log(fare);
+
+    const ride= rideModel.create({
+        user,
+        pickup,
+        destination,
+        otp: getOtp(6),
+        fare: fare[vehicleType]
+    })
+
+    return ride;
+}   
+
+
+
